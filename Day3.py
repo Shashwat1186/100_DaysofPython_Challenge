23b209b92b0b369478c308b82c7f6e3ec8655bbe
Shashwat Salil Warune <shashwatwarune@gmail.com>
2025-08-01 01:38:13 +0530

    Day5 and Day3 updated

--- Day3.py
+++ Day3.py
@@ -1,1 +1,18 @@
-print('YO')
+print("Welcome to the Treasure Island.\nYour mission is to find the treausre.")
+crossroad = input('''You are at a crossroad. Where do you want to go? \n       type "left" or "right"\n''').lower()
+if crossroad == "left" :
+    lake_crossing = input("""You've come to a lake. There is a island in the middle of the lake. \n  Type "wait" to wait for the boat. Type "swim" to swim across. \n""").lower()
+    if lake_crossing == "wait" :
+        colour = input("You arrive at the island unharmed. There is a house with 3 doors. \n One red, one blue and one yellow. Which colour do you choose? ").lower()
+        if colour == "yellow":
+            print("You win")
+        elif colour == "red":
+            print("You were burned by fire. Game over")
+        elif colour == "blue":
+            print("You were eaten by beasts. Game Over. ")
+        else :
+            print("Game over")
+    else :
+        print("You were attacked by a trout. Game over")
+else :
+    print("You fall into a hole. Game over")
---
+++ Day5.py
@@ -0,0 +1,21 @@
+import random
+letters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+']
+print("Welcome to the PyPassword Generator")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like in your password?\n"))
+nr_numbers = int(input("How many numbers would you like in your password?\n"))
+letters_pass = random.sample(letters, nr_letters)
+symbols_pass = random.sample(symbols, nr_symbols)
+numbers_pass = random.sample(numbers, nr_numbers)
+password = letters_pass + symbols_pass + numbers_pass
+print(password)
+random.shuffle(password)
+pas = ""
+for n in range(0, len(password)):
+    pas += password[n]
+print(pas)
